a443aff98f8e91a8c16f75fdaadfbb5fb48d55d2
emadmahouti <emad.mahouti@gmail.com>
2021-01-23 00:43:38 -0800

    Session fix

--- wwwroot/cache/99d4d63e0ccee2ca85db649a6ff3389734085f90.php
+++
@@ -1,6 +0,0 @@
-<?php $__env->startSection('content'); ?>
-<div style="display:flex; justify-content: space-around;" >
-	<h2>درحال‌حاضر سرویس از دسترس خارج است</h2>
-</div>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/cp36027/cafeoperations.ir/app/Views/deny.blade.php ENDPATH**/ ?>
